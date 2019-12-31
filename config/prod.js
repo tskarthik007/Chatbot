ab9dc486ef694c170da2d8ec422d6a8c018984a9
john <john@mymail.com>
2019-12-31 16:23:39 +0530

    connetion heroku and dialogflow

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
 	dialogFlowSessionID:process.env.DIALOGFLOW_SESSION_ID,
 	dialogFlowSessionLanguageCode:process.env.DIALOGFOW_LANGUAGE_CODE,
 	googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-	googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY
-};  
+	googlePrivateKey: JSON.prase(process.env.GOOGLE_PRIVATE_KEY)
+};
