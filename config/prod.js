ed88dfc986135ce5970c913c2cb9b27e333e2b96
john <john@mymail.com>
2019-12-31 19:13:31 +0530

    trying

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
 	dialogFlowSessionID:process.env.DIALOGFLOW_SESSION_ID,
 	dialogFlowSessionLanguageCode:process.env.DIALOGFOW_LANGUAGE_CODE,
 	googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-	googlePrivateKey: JSON.parser(process.env.GOOGLE_PRIVATE_KEY),
+	googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
 };
